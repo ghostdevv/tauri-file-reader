248cc5ac0bcab70987957ab793ff535a221afd18
GHOSt <ghostdevbusiness@gmail.com>
2022-09-24 01:05:41 +0100

    feat: code

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,21 @@
-Hello World
+<script>
+    import { readTextFile } from '@tauri-apps/api/fs';
+    import { open } from '@tauri-apps/api/dialog';
+
+    let contents;
+
+    async function openFile() {
+        const file = await open({
+            multiple: false,
+            directory: false,
+        });
+
+        contents = await readTextFile(file);
+    }
+</script>
+
+<button on:click={openFile}> Open File </button>
+
+{#if contents}
+    <pre>{contents}</pre>
+{/if}
