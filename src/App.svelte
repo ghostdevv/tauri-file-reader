a5d3415ccb7dd725369eea8e410dda7f6fdaf1c9
GHOSt <ghostdevbusiness@gmail.com>
2022-09-24 01:07:24 +0100

    chore: add guard

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import { readTextFile } from '@tauri-apps/api/fs';
     import { open } from '@tauri-apps/api/dialog';
 
+    /** @type {string} */
     let contents;
 
     async function openFile() {
@@ -9,6 +10,11 @@
             multiple: false,
             directory: false,
         });
+
+        if (typeof file != 'string') {
+            // No file given (also file won't be an array of strings here since multiple is false)
+            return;
+        }
 
         contents = await readTextFile(file);
     }
